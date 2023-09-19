94d27569f63e210984fc097362b92eb6ca18193e
marinadebora <m_deby_c@hotmail.com>
2023-09-19 18:36:19 -0200

    creando imagenes y contenedores de docker

--- Gateway/index.js
+++ Gateway/index.js
@@ -6,17 +6,17 @@ const app = express();
 app.use(morgan("dev"));
 
 app.use("/characters",createProxyMiddleware({
-  target:"http://localhost:8001",
+  target:"http://characters:8001",
   changeOrigin:true //indica que a partir de aca la request continua en este puerto
 
 }))
 app.use("/films",createProxyMiddleware({
-  target:"http://localhost:8002",
+  target:"http://films:8002",
   changeOrigin:true //indica que a partir de aca la request continua en este puerto
 
 }))
 app.use("/planets",createProxyMiddleware({
-  target:"http://localhost:8003",
+  target:"http://planets:8003",
   changeOrigin:true //indica que a partir de aca la request continua en este puerto
 
 }))
