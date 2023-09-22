0a7f2ac9ead9e9d041feab349109c0dad57f213f
Debora <m_deby_c@hotmail.com>
2023-09-22 15:14:12 -0300

    descomentando server

--- database/src/server.js
+++ database/src/server.js
@@ -6,13 +6,13 @@ server.use(express.json());
 server.use(morgan("dev"));
 
 server.use(require("./routes"));
-/* server.use("*",(req,res)=>res.status(400).send("Not found"));
+server.use("*",(req,res)=>res.status(400).send("Not found"));
 
 server.use((err,req,res,next)=>{
   res.status(err.statusCode || 500).send({
     error:true,
     message: err.message
   })
-}) */
+})
 
 module.exports = server;
