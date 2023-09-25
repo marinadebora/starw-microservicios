5bdc31c8fd4ecece229fd434516b13283e0bfa74
marinadebora <m_deby_c@hotmail.com>
2023-09-25 12:32:00 -0200

    conectando docker

--- Characters/src/controllers/getCharacters.js
+++ Characters/src/controllers/getCharacters.js
@@ -4,6 +4,6 @@ const { response } = require("../utils");
 
 module.exports = async (req, res) =>{
   const characters = await axios.get(`http://database:8004/Character`)
-  console.log(characters)
-  response(res, 200, characters)
+
+  response(res, 200, characters.data)
 }
--- Films/src/controllers/getFilms.js
+++ Films/src/controllers/getFilms.js
@@ -2,7 +2,7 @@ const { response }= require("../utils");
 const axios =require("axios")
 module.exports = async (req, res) =>{    
   const films =  await axios.get(`http://database:8004/Film`) 
-  response(res, 200, films)
+  response(res, 200, films.data)
  
  
 }
