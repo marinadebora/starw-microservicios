60489a8e0a1d37dea85c07583e0503dda79957e6
Debora <m_deby_c@hotmail.com>
2023-09-26 08:12:55 -0300

    database

--- database/index.js
+++ database/index.js
@@ -1,21 +1,4 @@
 const server = require("./src/server");
-const {Character,Film,Planet}= require("./src/dataBase")
-
-//Character.list().then((res)=>console.log(res));
-//Character.getId(1).then((res)=>console.log(res));
-
-/* Character.insert({
-    _id:"200",
-    name: "jorge vega",
-    height: "172",
-    mass: "77",
-    hair_color: "brown",
-    skin_color: "blond",
-    eye_color:"brown",
-    birth_year: "1991",
-    gender: "male"
-   
-}).then((res)=>console.log(res)) */
 
 server.listen(8004,()=>{
     console.log("database server on port 8004")
