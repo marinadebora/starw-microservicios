064377651f2ef6a936c5a22b9f57807a8e7f686a
Debora <m_deby_c@hotmail.com>
2023-09-26 15:36:10 -0300

    cambiando PORT

--- Database/index.js
+++ Database/index.js
@@ -1,8 +1,8 @@
 const server = require("./src/server");
-const {PORT_DATA_BASE} = require("./src/config/env")
+const PORT = 8004 
 
 
-server.listen(PORT_DATA_BASE,()=>{
-    console.log(`database server on port ${PORT_DATA_BASE}`);
+server.listen(PORT,()=>{
+    console.log(`database server on port ${PORT}`);
 })
 
